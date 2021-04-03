17bb9b2603b75227d584ed65d8460cc4b25ef1b1
tsukaryu <drgon8otsuka@gmail.com>
2021-04-03 22:05:07 +0900

    add aside

--- src/components/Contents/Contents.js
+++ src/components/Contents/Contents.js
@@ -7,11 +7,30 @@ import Travel from "./Travel/Travel";
 
 export default function Contents() {
   return (
-    <div>
+    <>
       <Hero />
-      <Travel />
-      <Houses />
-      <Popular />
-    </div>
+      <div className='mainContents'>
+        <aside className='side left'>
+          <div className='sideInner'>
+            <a href='#/' className='twitter icon'>
+              Twitter
+            </a>
+            <a href='#/' className='facebook icon'>
+              Facebook
+            </a>
+          </div>
+        </aside>
+        <main>
+          <Travel />
+          <Houses />
+          <Popular />
+        </main>
+      </div>
+      <aside className='side right'>
+        <div className='sideInner'>
+          <span>&copy;Code Mafia</span>
+        </div>
+      </aside>
+    </>
   );
 }
